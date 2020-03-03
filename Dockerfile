055b01bad6fc48ab738b6ce36f0aaa174f572f14
Jim Nilsson <jimnils@hotmail.com>
2020-03-03 20:30:55 +0100

    Add two cronjobs and other stuff

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:13.8.0-alpine3.11
 COPY package.json package-lock.json ./
 RUN npm ci
 
-COPY ./.env ./.env
+COPY ./.env.production ./.env
 COPY ./index.js ./index.js
 
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const Discord = require('discord.js')
 const fetch = require('node-fetch')
-require('dotenv').config()
+const CronJob = require('cron').CronJob
 const client = new Discord.Client()
 const { BOT_SECRET_TOKEN, NASA_API_KEY } = process.env
 
@@ -8,14 +9,26 @@ client.login(BOT_SECRET_TOKEN)
 
 client.on('ready', () => {
   console.log('Connected as ' + client.user.tag)
+  setupCronjobs(client.channels)
 })
+
+function broadcaster(msg) {
+  this.forEach(channel => {
+    if (channel.type !== 'text') {
+      return
+    }
+    channel.send(msg)
+  })
+}
+const broadcast = broadcaster.bind(client.channels)
 
 const commandMap = {
   '!ping': () => 'pong!',
   '!utbud': commandList,
   '!apod': apod,
-  '!roll': roll,
+  '!rulla': roll,
   '!korv': () => 'https://loremflickr.com/320/240/hotdog',
+  '!kris': crisis,
 }
 
 client.on('message', async msg => {
@@ -26,10 +39,6 @@ client.on('message', async msg => {
   }
 })
 
-function commandList() {
-  return `\n${Object.keys(commandMap).join('\n')}`
-}
-
 async function apod() {
   const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`
   const response = await fetch(url)
@@ -37,6 +46,74 @@ async function apod() {
   return json.hdurl + `\n${json.explanation}`
 }
 
+function commandList() {
+  return `\n${Object.keys(commandMap).join('\n')}`
+}
+
 function roll() {
-  return Math.floor(Math.random() * Math.floor(20))
+  const roll = Math.floor(Math.random() * (20 - 1 + 1) + 1)
+
+  if (roll === 20) {
+    return 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F736x%2Ffa%2F3a%2F08%2Ffa3a08031e524a4c6efa131c91078b6f.jpg&f=1&nofb=1'
+  }
+
+  if (roll === 1) {
+    return 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fih1.redbubble.net%2Fimage.280065502.4484%2Fflat%2C800x800%2C075%2Cf.jpg&f=1&nofb=1'
+  }
+
+  return roll
+}
+
+function setupCronjobs() {
+  const goodMorningCronJob = new CronJob('00 09 * * * *', () => {
+    broadcast('god morgon charkuterister')
+  })
+  const crisisCronJob = new CronJob('05 09 * * * *', async () => {
+    const msg = await crisis()
+    broadcast(msg)
+  })
+  goodMorningCronJob.start()
+  crisisCronJob.start()
+}
+
+async function crisis() {
+  const response = await fetch(
+    'http://api.krisinformation.se/v1/feed?format=json',
+  )
+  const json = await response.json()
+  const { Entries } = json
+  const utcNow = Date.now()
+  const entries = Entries.reduce((p, c) => {
+    const utcDate = dateToUtc(new Date(c.Published))
+    if (utcNow - 1000 * 60 * 60 * 24 > utcDate) {
+      return p
+    }
+    const output = `${new Date(c.Published).toLocaleDateString()} - **${
+      c.Title
+    }**
+    *${c.Summary}*`
+    return [...p, output]
+  }, [])
+
+  if (entries.length < 1) {
+    return 'Vill bara säga att det inte är någon kris! :sweat_smile:'
+  }
+
+  return `
+  >>> __***Krisinformation senaste 24h***__
+
+  ${entries.join('\n\n')}
+
+  `
+}
+
+function dateToUtc(date) {
+  return Date.UTC(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds(),
+  )
 }
